8c5344625ff46b0779c1befb76603e9ddeef4047
sendivogius <sendivogius@gmail.com>
2024-02-11 21:22:55 +0100

    add player healing

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -10,7 +12,6 @@ pub fn player_input(
     #[resource] turn_state: &mut TurnState,
 ) {
     let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
-
 
     if let Some(key) = key {
         let delta = match key {
@@ -24,7 +25,7 @@ pub fn player_input(
             .iter(ecs)
             .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
             .unwrap();
-
+        let mut did_something = false;
         if delta.x != 0 || delta.y != 0 {
             let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
             let mut hit_something = false;
@@ -35,13 +36,24 @@ pub fn player_input(
                 })
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
                     commands.push(((), WantsToAttack {
                         attacker: player_entity,
                         victim: *entity,
                     }));
                 });
             if !hit_something {
+                did_something = true;
                 commands.push(((), WantsToMove { entity: player_entity, destination }));
+            }
+        }
+
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>() {
+                health.current = i32::min(health.max, health.current + 1);
             }
         }
 
