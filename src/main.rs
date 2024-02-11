416cd6ec8fc24ac08c0659fdcdd44220841fa9e6
sendivogius <sendivogius@gmail.com>
2024-02-11 21:22:56 +0100

    Refactor to use templates

--- src/main.rs
+++ src/main.rs
@@ -47,9 +47,7 @@ impl State {
         // spawn_amulet_of_yala(&mut ecs, mb.amulet_start);
         let exit_idx = mb.map.point2d_to_index(mb.amulet_start);
         mb.map.tiles[exit_idx] = TileType::Exit;
-        mb.monster_spawns
-            .iter()
-            .for_each(|pos| spawn_entity(&mut ecs, &mut rng, *pos));
+        spawn_level(&mut ecs, &mut rng, 0, &mb.monster_spawns);
         resources.insert(mb.map);
         resources.insert(Camera::new(mb.player_start));
         resources.insert(TurnState::AwaitingInput);
@@ -122,12 +120,7 @@ impl State {
         // spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
         let exit_idx = map_builder.map.point2d_to_index(map_builder.amulet_start);
         map_builder.map.tiles[exit_idx] = TileType::Exit;
-        map_builder
-            .rooms
-            .iter()
-            .skip(1)
-            .map(|r| r.center())
-            .for_each(|pos| spawn_entity(&mut self.ecs, &mut rng, pos));
+        spawn_level(&mut self.ecs, &mut rng, 0, &map_builder.monster_spawns);
         self.resources.insert(map_builder.map);
         self.resources.insert(Camera::new(map_builder.player_start));
         self.resources.insert(TurnState::AwaitingInput);
@@ -185,10 +178,12 @@ impl State {
             map_builder.map.tiles[exit_id] = TileType::Exit;
         }
 
-        map_builder
-            .monster_spawns
-            .iter()
-            .for_each(|pos| spawn_entity(&mut self.ecs, &mut rng, *pos));
+        spawn_level(
+            &mut self.ecs,
+            &mut rng,
+            map_level as usize,
+            &map_builder.monster_spawns,
+        );
         self.resources.insert(map_builder.map);
         self.resources.insert(Camera::new(map_builder.player_start));
         self.resources.insert(TurnState::AwaitingInput);
