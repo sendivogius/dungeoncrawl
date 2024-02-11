78edf3f69036b693c87e94a1157c2333f6d63aab
sendivogius <sendivogius@gmail.com>
2024-02-11 21:22:55 +0100

    refactor: add wants move system

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -7,12 +7,13 @@ mod entity_renderer;
 mod map_renderer;
 mod player_input;
 mod random_move;
+mod movement;
 
 pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
         .flush()
-        .add_system(collisions::collisions_system())
+        .add_system(movement::movement_system())
         .flush()
         .add_system(map_renderer::map_render_system())
         .add_system(entity_renderer::entity_render_system())
@@ -31,6 +32,8 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(movement::movement_system())
+        .flush()
         .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_renderer::map_render_system())
--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -1,15 +1,16 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
     #[resource] turn_state: &mut TurnState,
 ) {
+    let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+
     if let Some(key) = key {
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
@@ -18,17 +19,10 @@ pub fn player_input(
             VirtualKeyCode::Down => Point::new(0, 1),
             _ => Point::zero(),
         };
-
-        if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query().filter(component::<Player>());
-            players.iter_mut(ecs).for_each(|pos| {
-                let desination = *pos + delta;
-                if map.can_enter_tile(desination) {
-                    *pos = desination;
-                    camera.on_player_move(desination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            })
-        }
+        players.iter(ecs).for_each(|(entity, pos)| {
+            let destination = *pos + delta;
+            commands.push(((), WantsToMove { entity: *entity, destination }));
+        });
+        *turn_state = TurnState::PlayerTurn;
     }
 }
