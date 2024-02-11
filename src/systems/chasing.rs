69f72b1e8e9ca747297996634d971803f40837be
sendivogius <sendivogius@gmail.com>
2024-02-11 21:22:55 +0100

    Use fov in chasing

--- src/systems/chasing.rs
+++ src/systems/chasing.rs
@@ -4,6 +4,7 @@ use crate::prelude::VirtualKeyCode::Scroll;
 #[system]
 #[read_component(Point)]
 #[read_component(ChasingPlayer)]
+#[read_component(FieldOfView)]
 #[read_component(Health)]
 #[read_component(Player)]
 pub fn chasing(
@@ -11,7 +12,7 @@ pub fn chasing(
     ecs: &SubWorld,
     commands: &mut CommandBuffer,
 ) {
-    let mut movers = <(Entity, &Point, &ChasingPlayer)>::query();
+    let mut movers = <(Entity, &Point, &ChasingPlayer, &FieldOfView)>::query();
     let mut positions = <(Entity, &Point, &Health)>::query();
     let mut player = <(&Point, &Player)>::query();
 
@@ -23,7 +24,10 @@ pub fn chasing(
         SCREEN_WIDTH, SCREEN_HEIGHT, &search_targets, map, 1024.0);
 
     movers.iter(ecs)
-        .for_each(|(entity, pos, _)| {
+        .for_each(|(entity, pos, _, fov)| {
+            if !fov.visible_tiles.contains(&player_pos) {
+                return;
+            }
             let idx = map_idx(pos.x, pos.y);
             if let Some(destination) = DijkstraMap::find_lowest_exit(&dijkstra_map, idx, map) {
                 let distance = DistanceAlg::Pythagoras.distance2d(*pos, *player_pos);
