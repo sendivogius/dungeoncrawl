67656516c93821e775db6eb1f8be27e2cc452efe
sendivogius <sendivogius@gmail.com>
2024-02-11 21:22:55 +0100

    add collisions

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -3,11 +3,13 @@ use crate::prelude::*;
 mod player_input;
 mod map_renderer;
 mod entity_renderer;
+mod collisions;
 
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(player_input::player_input_system())
         .add_system(map_renderer::map_render_system())
         .add_system(entity_renderer::entity_render_system())
+        .add_system(collisions::collisions_system())
         .build()
 }
